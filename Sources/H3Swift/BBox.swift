3641db5a3959619bbf1212eac94c1eef53cf430c
Teemu Harju <teemu.harju@gmail.com>
2019-10-24 07:07:33 +0300

    Added bboxHexRadius Swift wrapper.

--- Sources/H3Swift/BBox.swift
+++ Sources/H3Swift/BBox.swift
@@ -6,10 +6,17 @@
 //  Copyright © 2019 Shipyard Games Oy. All rights reserved.
 //
 
+import h3lib
+
 extension H3.BBox: Equatable {
     
     public static func == (lhs: H3.BBox, rhs: H3.BBox) -> Bool {
         return lhs.north == rhs.north && lhs.east == rhs.east &&
             lhs.south == rhs.south && lhs.west == rhs.west
+    }
+    
+    public func hexRadius(resolution: Int32) -> Int32 {
+        var bbox = self
+        return h3lib.bboxHexRadius(&bbox, resolution)
     }
 }
