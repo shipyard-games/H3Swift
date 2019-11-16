4f46bf8339eedb548580cd1ca55c113143ed2db0
Teemu Harju <teemu.harju@gmail.com>
2019-11-16 07:22:09 +0200

    Fixed tests.

--- Tests/H3SwiftTests/H3IndexTests.swift
+++ Tests/H3SwiftTests/H3IndexTests.swift
@@ -15,41 +15,44 @@ final class H3IndexTests: XCTestCase {
     
     func testGeoCoordToIndex() {
         
-        let geoCoord = H3.GeoCoord(lat: 40.689167, lon: -74.044444)
+        let geoCoord = H3.GeoCoord(latDegs: 40.689167, lonDegs: -74.044444)
         let index = geoCoord.toH3(resolution: 5)
         
         XCTAssertEqual(index.hexString, "0x852a1073fffffff")
     }
     
     func testIndexToGeoCoord() {
-        let geoCoord1 = H3.GeoCoord(lat: 40.72057966485312, lon: -74.09152905917784)
+        let geoCoord1 = H3.GeoCoord(latDegs: 40.72057966485312, lonDegs: -74.09152905917784)
         let index = geoCoord1.toH3(resolution: 5)
         
         let geoCoord2 = index.toGeo()
         
-        XCTAssertEqual(geoCoord1, geoCoord2)
+        XCTAssertEqual(geoCoord1.lat, geoCoord2.lat, accuracy: 0.000000001)
+        XCTAssertEqual(geoCoord1.lon, geoCoord2.lon, accuracy: 0.000000001)
     }
     
     func testRandomCellCenters() {
-        let geoCoord1 = H3.GeoCoord(lat: 10.752074718, lon: 345.714683533)
+        let geoCoord1 = H3.GeoCoord(latDegs: 10.752074718, lonDegs: 345.714683533)
         XCTAssertEqual(geoCoord1.toH3(resolution: 15).hexString, "0x8f54c4b5391d296")
         
-        let geoCoord2 = H3.GeoCoord(lat: -27.384130955, lon: 298.040864468)
+        let geoCoord2 = H3.GeoCoord(latDegs: -27.384130955, lonDegs: 298.040864468)
         XCTAssertEqual(geoCoord2.toH3(resolution: 15).hexString, "0x8fb2657092a5ca1")
         
-        let geoCoord3 = H3.GeoCoord(lat: -65.478402972, lon: 111.465804577)
+        let geoCoord3 = H3.GeoCoord(latDegs: -65.478402972, lonDegs: 111.465804577)
         XCTAssertEqual(geoCoord3.toH3(resolution: 15).hexString, "0x8fe5098d5d75d5c")
         
-        let geoCoord4 = H3.GeoCoord(lat: -3.635374904, lon: 155.61553359)
+        let geoCoord4 = H3.GeoCoord(latDegs: -3.635374904, lonDegs: 155.61553359)
         XCTAssertEqual(geoCoord4.toH3(resolution: 15).hexString, "0x8f76a1cc64caa8a")
     }
     
     func testToParentToChildren() {
-        let geoCoord = H3.GeoCoord(lat: 40.72057966485312, lon: -74.09152905917784)
+        let geoCoord = H3.GeoCoord(latDegs: 40.72057966485312, lonDegs: -74.09152905917784)
         let index = geoCoord.toH3(resolution: 10)
         
+        XCTAssertEqual(0x8a2a10700007fff, index)
+        
         let parent = index.toParent(resolution: 9)
-        XCTAssertEqual(parent.hexString, "0x896cb63a327ffff")
+        XCTAssertEqual(parent, 0x892a1070003ffff)
         
         let children = parent.toChildren(resolution: 10)
         XCTAssertEqual(children.count, 7)
